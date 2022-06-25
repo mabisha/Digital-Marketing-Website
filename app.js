1ff9671bb754992e74d0592a93cab096bd138eab
mabisha <dahalmabisha@gmail.com>
2022-06-25 19:40:29 +0545

    responsive nav-bar

---
+++ app.js
@@ -0,0 +1,11 @@
+let toggleBtn= document.querySelector(".toggle")
+let menu= document.querySelector(".menu")
+let closeBtn=document.querySelector(".close-btn")
+
+toggleBtn.addEventListener("click",  function() {
+  menu.classList.toggle("show-side-bar")
+});
+
+closeBtn.addEventListener("click",  function() {
+    menu.classList.remove("show-side-bar")
+  });
