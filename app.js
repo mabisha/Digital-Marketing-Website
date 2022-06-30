6c8f71cbd6274eb69bec01771794d50a81549f47
mabisha <dahalmabisha@gmail.com>
2022-06-30 21:18:08 +0545

    contact-page updated

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ toggleBtn.addEventListener("click",  function() {
 closeBtn.addEventListener("click",  function() {
     menu.classList.remove("show-side-bar")
   });
+function myFunction() {
+  var text;
+  text = "We have recieved your message. We will contact you later";
+  document.getElementById("myForm").style.display="none"
+  document.getElementById("submit-message").innerHTML = text;
 
- 
+  document.getElementById("submit-message").style.backgroundColor= "rgb(6, 168, 6)";
+  }
  
